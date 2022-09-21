e7c2546e3829d01221f2ce1447a806fc7c4fa84e
LittleSource <littlesourcer@gmail.com>
2022-09-21 10:14:22 +0800

    add footer text

--- components/Footer.vue
+++ components/Footer.vue
@@ -1,7 +1,9 @@
 <template>
 	<div class="footer">
 		<div class="text-sm">© 源梦科技 陇ICP备2022000468号-1</div>
-		<div class="mt-2 text-sm">服务条款 隐私条款</div>
+		<div class="mt-2 text-sm">
+			每个人都是一片小小泥土，在这片泥土上种植自己的生命之花。
+		</div>
 	</div>
 </template>
 
