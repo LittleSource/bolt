0e38471727e15c5260fb5fff1515b9f66893afb4
LittleSource <littlesourcer@gmail.com>
2022-09-26 15:46:27 +0800

    fix

--- components/content/ProseP.vue
+++ components/content/ProseP.vue
@@ -3,14 +3,10 @@ const slots = useSlots();
 const isImage = computed(() => {
 	return slots.default()[0].type.tag === "img";
 });
-const isLink = computed(() => {
-	return slots.default()[0].type.tag === "a";
-});
 </script>
 
 <template>
 	<div class="prose-image" v-if="isImage"><slot /></div>
-	<div class="prose-a" v-else-if="isLink"><slot /></div>
 	<p v-else><slot /></p>
 </template>
 
